5abcbd48e91255e7203b9290745f0476df55ec92
Faritha Nasreen <faritha@freshdesk.com>
2016-07-08 22:39:45 +0530

    Updated forms

--- GoodApp/app/controllers/user_controller.rb
+++ GoodApp/app/controllers/user_controller.rb
@@ -1,30 +1,31 @@
 class UserController < ApplicationController
-  @loginmsg="";
-  @signupmsg="";
-  def welcome
-    
-  end
+  #@loginmsg="";
+  #@signupmsg="";
 
   def create
   	#@user = User.find(params.require(:signup).permit(:username))
   	#if @user==nil
 	  	@user = User.new(params.require(:signup).permit(:fname, :lname, :username, :email, :password))
 	  	@user.save
-	  	@signupmsg = "You have Signedup Successfully with username : #{@user.username}"
+	  	#@signupmsg = "You have Signedup Successfully with username : #{@user.username}"
 	#else
 	#	@signupmsg = "Username already taken";
 	#end
 	#render plain: @user
-	redirect_to "homepage#show"
+	redirect_to "/show"
   end
 
   def login
-    @user = User.find(params[:username])
+    
+    @usern = User.find(params[:username])
     @userpass = User.find(params[:password])
 
-    if (@user.username == @userpass.username)
-    	redirect_to "homepage#show"
+    if (@usern.username == @userpass.username)
+      flash[:alert] = "Post successfully created"
+    	redirect_to "/show"
+
     else
+      redirect_to "/show"
     end
 
   end
--- GoodApp/config/routes.rb
+++ GoodApp/config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   get 'user/create'
 
   get 'user/login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/about', :to => 'authors#about'
   get '/books', :to => 'authors#books'

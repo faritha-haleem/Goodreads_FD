bc8ce7cd56e1e1f0cf5fbe5c58ecc6ceb059bc25
Faritha Nasreen <faritha@freshdesk.com>
2016-07-08 14:48:55 +0530

    Created home page UI and controller

---
+++ GoodApp/app/controllers/homepage_controller.rb
@@ -0,0 +1,5 @@
+class HomepageController < ApplicationController
+  def show
+    @title="Show"
+  end
+end
--- GoodApp/config/routes.rb
+++ GoodApp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'homepage/show'
+
   get 'publisher/show'
 
   get 'publisher/home'
@@ -18,4 +20,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/about', :to => 'authors#about'
   get '/books', :to => 'authors#books'
+  get '/show', :to => 'homepage#show'
 end

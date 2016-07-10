c011c3cf1f170f2f6d7ccda3a6992c9d14c89a2f
Faritha Nasreen <faritha@freshdesk.com>
2016-07-10 19:02:49 +0530

    Created UI for /books/index and /books/show

--- GoodApp/app/controllers/books_controller.rb
+++ GoodApp/app/controllers/books_controller.rb
@@ -8,7 +8,11 @@ class BooksController < ApplicationController
                .select("books.*, authors.name as author_name, publishers.pub_name as pub_name")
   end
   def show
-    @books = Book.find(params[:format])
+    @books1=Book.joins('LEFT OUTER JOIN book_authors ON book_authors.book_id = books.id
+                       LEFT OUTER JOIN publishers ON books.publisher_id = publishers.id
+                       LEFT OUTER JOIN authors ON authors.id = book_authors.author_id')
+               .select("books.*, authors.name as author_name, publishers.pub_name as pub_name")
+    @books = @books1.find(params[:format])
     @bookgenres = Genre.where(id: BookGenre.select(:genre_id).where("book_id" => @books.id))
     @reviews = Review.where(book_id: @books.id)
     @ratings = Rate.where(book_id: @books.id)
--- GoodApp/config/initializers/assets.rb
+++ GoodApp/config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += ['authors.css', 'bootstrap.css', 'js-image-slider.css', 'generic.css', 'js-image-slider.js', 'signup.js']
+Rails.application.config.assets.precompile += ['authors.css', 'bootstrap.css', 'js-image-slider.css', 'generic.css', 'js-image-slider.js', 'signup.js', 'star.js']
 Rails.application.config.assets.precompile += %w( mybook.css )
 
 # Add additional assets to the asset load path

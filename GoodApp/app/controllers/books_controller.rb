0636299633568ee39f74bed3da3fa3ea0f19040f
Faritha Nasreen <faritha@freshdesk.com>
2016-07-12 12:25:33 +0530

    Modified Books UI to display books grouped into genres.

--- GoodApp/app/controllers/books_controller.rb
+++ GoodApp/app/controllers/books_controller.rb
@@ -1,21 +1,28 @@
 class BooksController < ApplicationController
-
-  def index
+  def initialize
+    @books2=Book.joins('LEFT OUTER JOIN book_authors ON book_authors.book_id = books.id
+                       LEFT OUTER JOIN publishers ON books.publisher_id = publishers.id
+                       LEFT OUTER JOIN authors ON authors.id = book_authors.author_id
+                       LEFT OUTER JOIN book_genres ON book_genres.book_id = books.id
+                       LEFT OUTER JOIN genres on genres.id = book_genres.genre_id')
+                .select("books.*, authors.name as author_name, publishers.pub_name as pub_name, genres.id as genre_id")
 
     @books=Book.joins('LEFT OUTER JOIN book_authors ON book_authors.book_id = books.id
                        LEFT OUTER JOIN publishers ON books.publisher_id = publishers.id
                        LEFT OUTER JOIN authors ON authors.id = book_authors.author_id')
                .select("books.*, authors.name as author_name, publishers.pub_name as pub_name")
   end
+  def index
+  end
+  def booklist
+    @param1 = params[:param1]
+    @books3 = @books2.where("genre_id = ?",@param1)
+  end
   def show
-    @books1=Book.joins('LEFT OUTER JOIN book_authors ON book_authors.book_id = books.id
-                       LEFT OUTER JOIN publishers ON books.publisher_id = publishers.id
-                       LEFT OUTER JOIN authors ON authors.id = book_authors.author_id')
-               .select("books.*, authors.name as author_name, publishers.pub_name as pub_name")
-    @books = @books1.find(params[:format])
-    @bookgenres = Genre.where(id: BookGenre.select(:genre_id).where("book_id" => @books.id))
-    @reviews = Review.where(book_id: @books.id)
-    @ratings = Rate.where(book_id: @books.id)
+    @books1 = @books.find(params[:format])
+    @bookgenres = Genre.where(id: BookGenre.select(:genre_id).where("book_id" => @books1.id))
+    @reviews = Review.where(book_id: @books1.id)
+    @ratings = Rate.where(book_id: @books1.id)
   end
   def review
     @review = Review.new(params.require(:review).permit(:review_msg))
@@ -24,4 +31,10 @@ class BooksController < ApplicationController
 
   def rate
   end
+
+  def fiction
+    @param1 = params[:param1]
+    @books3 = @books2.where("genre_id = ?",@param1)
+  end
+
 end
--- GoodApp/config/routes.rb
+++ GoodApp/config/routes.rb
@@ -34,6 +34,9 @@ Rails.application.routes.draw do
 
   get 'genre/show'
 
+  get 'books/fiction'
+
+  get 'books/booklist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/about', :to => 'authors#about'
   get '/books', :to => 'authors#books'

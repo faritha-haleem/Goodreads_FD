e232edf3a1e1f7541227764b5d97d180231076f1
irfu572 <shaik@freshdesk.com>
2016-07-12 23:50:08 +0530

    routes changes after conflict

--- GoodApp/config/routes.rb
+++ GoodApp/config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
 
   get 'books/:id' => 'books#show', as: :book
 
+
   post 'books/review'
 
   get 'auth/:provider/callback', to: 'sessions#create'
@@ -34,15 +35,21 @@ Rails.application.routes.draw do
 
   root 'homepage#show'
 
+  get 'user/create'
+
+  post 'user/login'
+
+  get 'user/show'
+
   get 'genre/show'
+
+  get 'books/fiction'
 
   get 'books/booklist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/about', :to => 'authors#about'
   get '/books', :to => 'authors#books'
-
   get '/show', :to => 'homepage#show'
-
   get '/review',:to => 'books#review'
 
   get '/show' => 'homepage#show'
